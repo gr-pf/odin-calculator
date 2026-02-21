fc85c8b4b1eff2318f444529c59497b4b60f38c4
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-02-21 12:32:44 +0100

    Complete the addition of the calculator's basic functionalities

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// Logic calculator functions
 export function add(a, b) {
   return +a + +b;
 }
@@ -18,6 +19,7 @@ export function operate(operand1, operand2, operator) {
   return operator(operand1, operand2);
 }
 
+// Display calcultator functions
 export function showFormula(text) {
   const display = document.querySelector(".formula");
   display.textContent = text;
@@ -26,4 +28,54 @@ export function showFormula(text) {
 export function showResult(text) {
   const display = document.querySelector(".result");
   display.textContent = text;
+}
+
+export function getStackText(stack) {
+  switch (stack.length) {
+    case 1:
+      return `${stack[0]}`;
+    case 2:
+      return `${stack[0]} ${stack[1]}`;
+    case 3:
+      return `${stack[0]} ${stack[1]} ${stack[2]}`;
+    default:
+      return "Erreur : stack vide !";
+  }
+}
+
+// EventListener callback functions
+
+/**
+ *
+ * @param {string} key
+ * @param {string} operand
+ * @returns {string} updatedOperand
+ */
+export function updateOperand(key, operand) {
+  let updatedOperand;
+  switch (key) {
+    case "+/-":
+      operand[0] === " "
+        ? (updatedOperand = "-" + operand.slice(1))
+        : (updatedOperand = " " + operand.slice(1));
+      break;
+
+    case "0":
+      operand.length === 2 && operand[1] === "0"
+        ? (updatedOperand = operand)
+        : (updatedOperand = operand + "0");
+      break;
+
+    case ".":
+      operand.includes(".")
+        ? (updatedOperand = operand)
+        : (updatedOperand = operand + ".");
+      break;
+
+    default:
+      operand.length === 2 && operand[1] === "0"
+        ? (updatedOperand = operand[0] + key)
+        : (updatedOperand = operand + key);
+  }
+  return updatedOperand;
 }
--- script.js
+++ script.js
@@ -6,23 +6,79 @@ import {
   operate,
   showFormula,
   showResult,
+  getStackText,
+  updateOperand,
 } from "./functions.js";
 
-let formula = {
-  operand1: " ",
-  operand2: " ",
-  operator: "",
+let operand1 = " 0";
+let operand2 = " 0";
+let operator = "";
+let postEval = false;
+let stackOperations = [operand1];
+const operatorFunctions = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
 };
 
 const keys = document.querySelectorAll("a");
 
 keys.forEach((elem) =>
   elem.addEventListener("click", function (event) {
-    formula.operand1 += event.currentTarget.textContent;
-    showFormula(`${formula.operand1} ${formula.operator} ${formula.operand2}`);
+    const keyClass = event.target.className.split(" ").at(0);
+    const keyContent = event.target.textContent;
+    switch (keyClass) {
+      case "operand":
+        if (postEval) {
+          operand1 = updateOperand(keyContent, " 0");
+          stackOperations[0] = operand1;
+          postEval = false;
+        } else if (stackOperations.length === 1) {
+          operand1 = updateOperand(keyContent, operand1);
+          stackOperations[0] = operand1;
+        } else {
+          operand2 = updateOperand(keyContent, operand2);
+          stackOperations[2] = operand2;
+        }
+        break;
+
+      case "operator":
+        postEval = false;
+        if (stackOperations.length < 3) {
+          operator = keyContent;
+          stackOperations[1] = operator;
+        } else {
+          operand1 = operate(operand1, operand2, operatorFunctions[operator]);
+          operand2 = " 0";
+          operator = keyContent;
+          stackOperations = [operand1, operator];
+          showResult(operand1);
+        }
+        break;
+
+      case "eval":
+        if (stackOperations.length === 3) {
+          operand1 = operate(operand1, operand2, operatorFunctions[operator]);
+          operand2 = " 0";
+          operator = "";
+          stackOperations = [operand1];
+        }
+        showResult(operand1);
+        postEval = true;
+        break;
+
+      case "clear":
+        operand1 = " 0";
+        operand2 = " 0";
+        operator = "";
+        postEval = false;
+        stackOperations = [operand1];
+        showResult("0");
+        break;
+    }
+    console.log(operand1);
+    console.log(stackOperations);
+    showFormula(getStackText(stackOperations));
   }),
 );
-// const key7 = document.querySelector("#k-7");
-// console.log(key7);
-// operand1 += key7.textContent;
-showFormula(`${formula.operand1} ${formula.operator} ${formula.operand2}`);
